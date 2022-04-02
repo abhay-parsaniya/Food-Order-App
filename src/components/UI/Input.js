3591d67c9c53b8afdd38055fcc02b148ed60ed2b
abhay.parsaniya <abhay.parsaniya@bacancy.com>
2022-04-02 13:21:46 +0530

    Food Order App

---
+++ src/components/UI/Input.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { forwardRef } from 'react/cjs/react.production.min';
+import classes from './Input.module.css';
+
+const Input = forwardRef((props, ref) => {
+  return (
+    <>
+        <div className={classes.input}>
+            <label htmlFor={props.input.id} >{props.label}</label>
+            <input ref={ref} {...props.input} />
+        </div>
+    </>
+  );
+});
+
+export default Input;
